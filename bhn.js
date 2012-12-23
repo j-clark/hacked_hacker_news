4332fdbaec99dfecfa8b66f2ffd310fc91acaf62
Joshua Clark <joshua.clark@txstate.edu>
2012-12-23 12:48:04 -0600

    renamed better_hn.css to bhn.css, updated manifest.json to get the extension to load, posts on each page now go into localStorage

--- bhn.js
+++ bhn.js
@@ -1,3 +1,16 @@
 ;(function() {
-  alert('hello world');
+  $(function() {
+
+    $('span[id^="score_"]').each(function() {
+      addToLocalStorage(this.id.split('_')[1]);
+    });
+
+  });
+
+  function addToLocalStorage(postID) {
+    if(!localStorage.getItem(postID)) {
+      localStorage.setItem(postID, []);
+    }
+  }
+
 })();
